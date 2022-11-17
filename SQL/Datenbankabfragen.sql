4807bb578cb2893238963c4f0478bf0f7ecf52e2
julienco03 <julian160903@gmail.com>
2022-11-17 12:51:03 +0100

    Add file for database queries

---
+++ SQL/Datenbankabfragen.sql
@@ -0,0 +1,24 @@
+/** Wie viele Ferienwohnungen wurden noch nie gebucht? */
+
+/** Welche Kunden haben eine Ferienwohnung bereits mehrmals gebucht? */
+
+/** Welche Ferienwohnungen in Spanien haben durchschnittlich mehr als 4 Sterne erhalten? */
+
+/** Welche Ferienwohnung hat die meisten Ausstattungen? Falls mehrere Ferienwohnungen das
+    Maximum an Ausstattungen erreichen, sollen diese alle ausgegeben werden. */
+
+/** Wie viele Reservierungen gibt es für die einzelnen Länder? Länder, in denen keine Reservierungen
+    existieren, sollen mit der Anzahl 0 ebenfalls aufgeführt werden. Das Ergebnis soll nach der Anzahl
+    Reservierungen absteigend sortiert werden. */
+
+/** Wie viele Ferienwohnungen sind pro Stadtnamen gespeichert? */
+
+/** Untersuchen Sie, ob es Doppelbuchungen gibt. Geben Sie dazu alle Ferienwohnungen aus, für die es
+    zwei Buchungen gibt, die die Ferienwohnung am gleichen Tag reservieren. Hinweis: es ist kein
+    Problem, wenn ein Kunde die Ferienwohnung verlässt, und am gleichen Tag ein anderer Kunde
+    einzieht. */
+
+/** Welche Ferienwohnungen mit Sauna sind in Spanien in der Zeit vom 1.11.2022 – 21.11.2022 noch
+    frei? Geben Sie den Ferienwohnungs-Namen und deren durchschnittliche Bewertung an.
+    Ferienwohnungen mit guten Bewertungen sollen zuerst angezeigt werden. Ferienwohnungen ohne
+    Bewertungen sollen am Ende ausgegeben werden. */
